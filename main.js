eee344cb3e4ece107806d2c920701fc86ddd86c0
Kenneth Wang <yunfeiw@me.com>
2021-09-16 22:48:44 -0700

    add speed control and accel

--- main.js
+++ main.js
@@ -3,54 +3,79 @@ var data = {
   facing: 'east',
   x: 0,
   y: 0,
-  speed: 0
+  speed: 2
 };
 var timeId = null;
-var movement = 0;
+var $speedDisplay = document.querySelector('.speed');
+var accl = 0;
 
 function turnCar(event) {
   if (event.key === 'ArrowUp') {
     $carHolder.className = 'carHolder north';
     data.facing = 'north';
-    movement = 0;
+
   } else if (event.key === 'ArrowRight') {
     $carHolder.className = 'carHolder';
     data.facing = 'east';
-    movement = 0;
+
   } else if (event.key === 'ArrowLeft') {
     $carHolder.className = 'carHolder west';
     data.facing = 'west';
-    movement = 0;
+
   } else if (event.key === 'ArrowDown') {
     $carHolder.className = 'carHolder south';
     data.facing = 'south';
-    movement = 0;
+
   }
 
 }
 
 function moveCar(event) {
-  movement += 0.02;
 
   if (data.facing === 'east') {
 
-    $carHolder.style.left = (parseInt($carHolder.style.left) + movement) + 'px';
+    $carHolder.style.left = data.x + 'px';
+    data.x += data.speed;
+  }
+  if (data.facing === 'south') {
+
+    $carHolder.style.top = data.y + 'px';
+    data.y += data.speed;
+  }
+
+  if (data.facing === 'west') {
+
+    $carHolder.style.left = data.x + 'px';
+    data.x -= data.speed;
+  }
+  if (data.facing === 'north') {
+
+    $carHolder.style.top = data.y + 'px';
+    data.y -= data.speed;
+  }
+}
+
+function accCar() {
+  accl += 0.05;
+  if (data.facing === 'east') {
+
+    $carHolder.style.left = (data.x + accl) + 'px';
 
   }
   if (data.facing === 'south') {
 
-    $carHolder.style.top = (parseInt($carHolder.style.top) + movement) + 'px';
+    $carHolder.style.top = (data.y + accl) + 'px';
 
   }
 
   if (data.facing === 'west') {
 
-    $carHolder.style.left = (parseInt($carHolder.style.left) - movement) + 'px';
+    $carHolder.style.left = (data.x - accl) + 'px';
 
   }
   if (data.facing === 'north') {
 
-    $carHolder.style.top = (parseInt($carHolder.style.top) - movement) + 'px';
+    $carHolder.style.top = (data.y - accl) + 'px';
 
   }
   data.x = parseInt($carHolder.style.left);
@@ -58,11 +83,25 @@ function moveCar(event) {
 }
 
 function autoDrive() {
+  clearInterval(timeId);
   timeId = setInterval(moveCar, 16);
 }
 
+function autoDriveAccl() {
+  clearInterval(timeId);
+  timeId = setInterval(accCar, 16);
+}
 function stopDrive() {
   clearInterval(timeId);
+}
+
+function speedChange(event) {
+
+  if (Number.isInteger(Number(event.key)) === false) {
+    return;
+  }
+  data.speed = Number(event.key);
+  $speedDisplay.textContent = 'Speed: ' + data.speed;
 }
 
 document.addEventListener('keydown', function () {
@@ -73,7 +112,14 @@ document.addEventListener('keydown', function () {
   if (event.key === 'b') {
     stopDrive();
   }
+
+  if (event.key === 'a') {
+    autoDriveAccl();
+  }
+
   turnCar(event);
+  speedChange(event);
+
 });
 
 function carPlace() {
